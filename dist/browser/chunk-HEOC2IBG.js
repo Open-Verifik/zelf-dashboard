759a71723cdf278af81b6f833ba949ed36957f2d
migueltrevinom <miguel@verifik.co>
2026-03-09 15:00:31 -0500

    lawyer integration

---
+++ dist/browser/chunk-HEOC2IBG.js
@@ -0,0 +1,1 @@
+import"./chunk-TSRGIXR5.js";var e=[{path:"",redirectTo:"lawyers",pathMatch:"full"},{path:"lawyers",loadComponent:()=>import("./chunk-Y6XLF4WJ.js").then(t=>t.LawyersComponent)}];export{e as default};
